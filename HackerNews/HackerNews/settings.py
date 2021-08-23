4aebdbab9df33d76078d3d00cccea61c0f8b83ca
Vadym <vadym.horval@gmail.com>
2021-08-23 07:52:24 +0300

    init1

--- HackerNews/HackerNews/settings.py
+++ HackerNews/HackerNews/settings.py
@@ -141,6 +141,7 @@ django_heroku.settings(locals())
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
