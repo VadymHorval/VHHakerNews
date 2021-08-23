6a3b1d123b19e55e508a549eb904fdef278bdc4d
Vadym <vadym.horval@gmail.com>
2021-08-23 07:51:10 +0300

    init1

--- HackerNews/HackerNews/settings.py
+++ HackerNews/HackerNews/settings.py
@@ -138,7 +138,9 @@ STATIC_URL = "/static/"
 DISABLE_COLLECTSTATIC=1
 
 django_heroku.settings(locals())
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
